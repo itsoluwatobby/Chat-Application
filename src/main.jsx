ac300523267cf4e34a9864825b0277e91fde6533
itsoluwatobby <oluwasamuel050@gmail.com>
2023-02-10 02:59:40 +0100

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import {App} from './App'
 import './index.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ChatContextProvider } from './context/ChatContext';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+process.env.NODE_ENV === 'production' && disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode> 
